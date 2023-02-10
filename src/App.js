ac537448481b2e080e307533042e97a912f117de
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-02-10 11:51:29 +0530

    added material UI to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
 }
 
 export default App;
