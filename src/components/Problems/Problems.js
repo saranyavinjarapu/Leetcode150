ab32a578864a8437f40224d4ff78a79beb0ca4b9
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-02-28 13:29:07 +0530

    readjusted css for problem table

--- src/components/Problems/Problems.js
+++ src/components/Problems/Problems.js
@@ -1,15 +1,9 @@
 import React from "react";
 import styles from "./Problems.module.css";
 import ProblemsList from "./ProblemsList";
-import ProblemDescription from "./ProblemDescription";
 
 const Problems = () => {
-  return (
-    <div className={styles.Problems}>
-      <ProblemsList></ProblemsList>
-      <ProblemDescription></ProblemDescription>
-    </div>
-  );
+  return <ProblemsList></ProblemsList>;
 };
 
 export default Problems;
